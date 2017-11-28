c7c2d5848e0e062e1942d2bd8341d6ec1dc27e0f
Filippo Gorini <filippo.gorini@gmail.com>
2017-11-28 16:22:20 +0100

    add new source

--- src/js/be.js
+++ src/js/be.js
@@ -2,15 +2,35 @@ $(document).ready(function() {
     //init scrollmagic
     var controller = new ScrollMagic.Controller();
     
+    // pin the intro
+
+	var pinIntroScene = new ScrollMagic.Scene({
+    	triggerElement: '.intro',
+    	triggerHook: 0,
+    	duration: '30%'
+    })
+    .setPin('.intro', {
+    	pushFollowers: false
+    })
+    .addTo(controller);
+
+	var pinIntroScene = new ScrollMagic.Scene({
+    	triggerElement: '.main',
+    	triggerHook: 0.4
+    })
+    .setPin('.intro', {
+    	pushFollowers: false
+    })
+    .addTo(controller);
 
     //loop
     $('.box').each(function(){
-    	
+
 	    // build a scene
 	    var ourScene = new ScrollMagic.Scene({
 	        triggerElement: this.children[0],
 	        duration: 400,
-	        triggerHook: 0.1
+	        triggerHook: 0.2
 
 	    })
 	    .setClassToggle(this, 'fade-in')
